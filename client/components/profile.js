8856e628dc46f0843be0e2c6a2285981f88c2f36
Vitaly K <43697710+shut56@users.noreply.github.com>
2020-09-24 15:21:44 +0500

    Update profile.js

--- client/components/profile.js
+++ client/components/profile.js
@@ -12,16 +12,15 @@ const Profile = () => {
         className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10"
       >
         Profile
-        <div>
-          <Link to="/dashboard/">Go To Root</Link>
-        </div>
-        <div>
-          <Link to="/dashboard/main">Go to Main</Link>
-        </div>
-        <div id="username">{user}</div>
       </div>
+      <div>
+        <Link to="/dashboard/">Go To Root</Link>
+      </div>
+      <div>
+        <Link to="/dashboard/main">Go to Main</Link>
+      </div>
+      <div id="username">{user}</div>
     </div>
-    //
   )
 }
 
