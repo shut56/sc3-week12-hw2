7abfbf2471321c8d5421daa76ee5559cdeb1f83e
Vitaly K <43697710+shut56@users.noreply.github.com>
2020-09-24 15:09:55 +0500

    target blank deleted

--- client/components/header.js
+++ client/components/header.js
@@ -28,8 +28,6 @@ const Header = () => {
         <div id="menu" className="block">
           <a
             href="https://www.google.com"
-            target="_blank"
-            rel="noopener noreferrer"
             className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white"
           >
             <svg
