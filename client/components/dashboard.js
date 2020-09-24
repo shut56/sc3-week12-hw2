a9c0b216f28086db87da5155c70ac6629b58060f
Vitaly K <43697710+shut56@users.noreply.github.com>
2020-09-24 15:19:21 +0500

    Update dashboard

--- client/components/dashboard.js
+++ client/components/dashboard.js
@@ -8,15 +8,15 @@ const Dashboard = () => {
       <Head title="Hello" />
       <div
         id="title"
-        className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10"
+        className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-2"
       >
         Dashboard
-        <div>
-          <Link to="/dashboard/profile/385666b1-bff5-11e9-95ba-1bf845c18f8d">Go to Profile</Link>
-        </div>
-        <div>
-          <Link to="/dashboard/main">Go to Main</Link>
-        </div>
+      </div>
+      <div>
+        <Link to="/dashboard/profile/385666b1-bff5-11e9-95ba-1bf845c18f8d">Go to Profile</Link>
+      </div>
+      <div>
+        <Link to="/dashboard/main">Go to Main</Link>
       </div>
     </div>
   )
