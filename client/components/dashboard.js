9f5f59cd12f05f78b950cedc85a7ba2fabe26f2c
Vitaly K <43697710+shut56@users.noreply.github.com>
2020-09-24 14:53:07 +0500

    Initial commit

---
+++ client/components/dashboard.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import Head from './head'
+
+const Dashboard = () => {
+  return (
+    <div>
+      <Head title="Hello" />
+      <div
+        id="title"
+        className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10"
+      >
+        Dashboard
+        <div>
+          <Link to="/dashboard/profile/385666b1-bff5-11e9-95ba-1bf845c18f8d">Go to Profile</Link>
+        </div>
+        <div>
+          <Link to="/dashboard/main">Go to Main</Link>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+Dashboard.propTypes = {}
+
+export default Dashboard
+
+/*
+Внутри должно быть три элемента
+
+div с id="title" и текстом Dashboard
+ссылка(реакт) с навигацией /dashboard/profile/92589c47-3214-4bd9-bc56-f53706d94798 и 
+текстом "Go To Profile"
+ссылка(реакт) с навигацией /dashboard/main и текстом "Go To Main"
+*/
---
+++ client/components/profile.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { Link, useParams } from 'react-router-dom'
+import Head from './head'
+//
+const Profile = () => {
+  const { user } = useParams()
+  return (
+    <div>
+      <Head title="Hello" />
+      <div
+        id="title"
+        className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10"
+      >
+        Profile
+        <div>
+          <Link to="/dashboard/">Go To Root</Link>
+        </div>
+        <div>
+          <Link to="/dashboard/main">Go to Main</Link>
+        </div>
+        <div id="username">{user}</div>
+      </div>
+    </div>
+    //
+  )
+}
+
+Profile.propTypes = {}
+
+export default Profile
+
+/*
+Внутри должно быть четыре элемента
+
+ссылка(реакт) с навигацией /dashboard и текстом "Go To Root"
+ссылка(реакт) с навигацией /dashboard/main и текстом "Go To Main"
+div с id="title" и текстом Profile
+div c id="username" и текстом, которые берется из параметра роута(см лекцию)
+надо использовать
+
+import { useParams } from 'react-router-dom'  
+
+const { username } = useParams()  
+*/
